bd2dbca14dc478b5b142fb7aa8cfadc685eed0e7
kennyosc <kennyoscar95@gmail.com>
2019-06-21 11:30:21 +0700

    add function onButtonClick

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,31 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class Register extends Component{
+    //ini harus dibuat dengan ARROW FUNCTION, karena this di dalamnya akan mengacu pada object
+    //kalau pakai function() biasa, this nya akan mengacu di tempat function ini terpanggil
+    onButtonClick = () =>{
+        const username = this.username.value
+        const email = this.email.value
+        const pass = this.password.value
+
+        //Taruh data di JSON dengan menggunakan AXIOS
+        //Untuk POST data mengunakan axios.post
+        //untuk mengambil data, menggunakan axios.get
+        axios.post('http://localhost:2019/users',{
+            user : username,
+            email: email,
+            password: pass
+        }).then((res)=>{
+            console.log(res)
+            console.log('Data berhasil diinput')
+        }).catch((err)=>{
+            console.log(err)
+            console.log('Gagal post data')
+        })
+        //.then itu kalau berhaasil, .catch itu kalau gagal
+    }
+
     render(){
         return(
            <div>
@@ -18,7 +43,7 @@ class Register extends Component{
                             </div>
                             <form className='input-group'>
                                 <input className='form-control'
-                                ref={(input) => {this.username = input}}></input>
+                                ref={(username) => {this.username = username}}></input>
                             </form>
 
                             <div className='card-title'>
@@ -26,7 +51,7 @@ class Register extends Component{
                             </div>
                             <form className='input-group'>
                                 <input className='form-control'
-                                ref={(input) => {this.email = input}}></input>
+                                ref={(email) => {this.email = email}}></input>
                             </form>
 
                             <div className='card-title'>
@@ -34,10 +59,10 @@ class Register extends Component{
                             </div>
                             <form className='input-group'>
                                 <input className='form-control' type="password"
-                                ref={(input) => {this.password = input}}></input>
+                                ref={(password) => {this.password = password}}></input>
                             </form>
                             
-                            <button className='btn btn-success mt-3'>Register</button>
+                            <button className='btn btn-success mt-3' onClick={this.onButtonClick}>Register</button>
                         </div>
                     </div>
                 </div>
