2a7c63fb6a2ccf29a468f728094d2acd2b09b10d
kennyosc <kennyoscar95@gmail.com>
2019-06-21 09:51:19 +0700

    add initial index and component, i bootstrap, i redux react-redux axios

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Register from './components/Register'
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1>App Component</h1>
+                <Register/>
+            </div>
+        )
+    }
+}
+
+export default App
