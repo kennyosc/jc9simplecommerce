f1d80031ba60b9c0bb6dfe999f213d86050e8a28
kennyosc <kennyoscar95@gmail.com>
2019-06-24 10:45:52 +0700

    add file Header, Home, Login, ManagerProduct

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react'
+
 import Register from './Register'
+import Header from './Header'
+import Home from './Home'
+import Login from './Login'
+import ManageProduct from './ManageProduct'
 
 class App extends Component{
     render(){
