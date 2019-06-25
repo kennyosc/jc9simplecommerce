b94850e89867e617c7566b0c593f832a169c36e2
kennyosc <kennyoscar95@gmail.com>
2019-06-25 10:00:35 +0700

    add login button logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,30 @@
 import React,{Component} from 'react'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 class Login extends Component{
+
+    onButtonClick = () =>{
+        var username = this.username.value;
+        var password = this.password.value;
+
+        axios.get("http://localhost:2019/users", {
+            params:{
+                username: username,
+                password : password
+            }
+        }).then((res)=>{
+            console.log(res)
+            for(var i = 0; i<res.data.length; i++){
+                if((res.data[i].username !== username && res.data[i].password !== password) || res.data.length === 0){
+                    console.log("Username/ Password anda salah")
+                } else if (res.data[i].username == username && res.data[i].password == password){
+                    console.log('You have been logged in')
+                }
+            }
+        })
+    }
+
     render(){
         return(
            <div>
@@ -29,7 +52,7 @@ class Login extends Component{
                                 <input className='form-control' type="password"
                                 ref={(password) => {this.password = password}}></input>
                             </form>
-                            <button className='btn btn-primary mt-3' onClick={this.onButtonClicks}>Login</button>
+                            <button className='btn btn-primary mt-3' onClick={this.onButtonClick}>Login</button>
                             <p className="mt-3">Tidak memiliki akun? <Link to="/register">Register disini</Link></p>
 
                         </div>
