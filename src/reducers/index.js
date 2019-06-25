e675323ef699562d310dfc11b130e60e0a5ad79b
kennyosc <kennyoscar95@gmail.com>
2019-06-25 12:04:10 +0700

    add authReducer logic in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,8 +8,14 @@ const init = {
 const AuthReducer = (data = init, action) =>{
     switch (action.type) {
         case "LOGIN_SUCCESS":
-            return (action.payload)
-            break;
+            return{
+                ...data, //OBJECT pada dasarnya akan mengganti keseluruhan object sebelumnya, jika di assign value baru
+                // ... data akan mengambil semua property dari data sebelumnya dan dimasukkan ke dalam object baru
+                // sehingga properti data yang lama tidak hilang
+                id: action.payload.id,
+                username: action.payload.username
+            };
+        break;
     
         default:
             return data
