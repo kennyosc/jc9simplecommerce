7df8412f3a4ce76b2d8261220d95b12439bc124d
kennyosc <kennyoscar95@gmail.com>
2019-06-26 09:50:52 +0700

    add if sign in logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,6 +16,7 @@ import {
     DropdownItem } from 'reactstrap'
     
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 
 class Header extends Component{
@@ -34,34 +35,79 @@ class Header extends Component{
       }
 
     render(){
+      if(this.props.user.username == ''){
         return(
-            <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">SimpleCommerce</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem className="my-2">
-                <Link to="/components/">All Products</Link>
-              </NavItem>
-              <NavItem>
-                  {/* karena Button in digunakan untuk ke link, makanya di wrap dengan <Link></Link> */}
-                <Link to="/register">
-                    <Button color="outline-success" className="mx-3">Register</Button>
-                </Link>
-              </NavItem>
-              <NavItem>
-                  <Link to='/login'>
-                    <Button color="outline-primary">Login</Button>
-                  </Link>
-              </NavItem>
-              
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+          <div>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">SimpleCommerce</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem className="my-2">
+                    <Link to="/components/">All Products</Link>
+                  </NavItem>
+                  <NavItem>
+                      {/* karena Button in digunakan untuk ke link, makanya di wrap dengan <Link></Link> */}
+                    <Link to="/register">
+                        <Button color="outline-success" className="mx-3">Register</Button>
+                    </Link>
+                  </NavItem>
+                  <NavItem>
+                      <Link to='/login'>
+                        <Button color="outline-primary">Login</Button>
+                      </Link>
+                  </NavItem>
+                  
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>
         )
+      } else {
+          return (
+            <div>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">SimpleCommerce</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                <NavItem className="my-2">
+                    <Link to="/manageproduct/" className="mx-3">Hello {this.props.user.username}</Link>
+                  </NavItem>
+                  <NavItem className="my-2">
+                    <Link to="/manageproduct/" className="mx-3">All Products</Link>
+                  </NavItem>
+                    <UncontrolledDropdown nav inNavbar>
+                              <DropdownToggle nav caret>
+                                Options
+                              </DropdownToggle>
+                              <DropdownMenu right>
+                                <DropdownItem>
+                                  OVO
+                                </DropdownItem>
+                                <DropdownItem>
+                                  Add Product
+                                </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                  SignOut
+                                </DropdownItem>
+                              </DropdownMenu>
+                            </UncontrolledDropdown>
+                            </Nav>
+              </Collapse>
+            </Navbar>
+          </div>
+                  
+        )
+      }
     }
 }
 
-export default Header
+const mapStateToProps = (state) =>{
+  return{
+    user : state.auth
+  }
+}
+
+export default connect(mapStateToProps)(Header)
--- src/components/Login.js
+++ src/components/Login.js
@@ -18,7 +18,6 @@ class Login extends Component{
     render(){
         return(
            <div>
-               <h1>{this.props.user.username}</h1>
                {/* col-sm-3 akan bisa dijalankan ketika atasnya ada class ROW */}
                 <div className = 'mt-5 row'>
                     <div className = "col-sm-3 mx-auto card">
